548c66f3c31d9f866d3ce5b78b67a82571fc565b
Cesare de Cal <contact@cesare.io>
2018-12-19 13:37:02 +0100

    Implementing cards controller

--- LanguageTranslationAR/LanguageTranslationAR/Controllers/Cards/CardsController.swift
+++ LanguageTranslationAR/LanguageTranslationAR/Controllers/Cards/CardsController.swift
@@ -9,54 +9,94 @@
 import UIKit
 import ZLSwipeableViewSwift
 
-class CardsController: UIViewController {
+class CardsController: ZLSwipeableViewController {
+}
+
+class ZLSwipeableViewController: UIViewController {
     
-    let swipeableView: ZLSwipeableView = {
-        let cv = ZLSwipeableView()
-        cv.translatesAutoresizingMaskIntoConstraints = false
-        return cv
-    }()
+    var swipeableView: ZLSwipeableView!
     
-    let arView: CustomView = {
-        let arView = CustomView()
-        arView.translatesAutoresizingMaskIntoConstraints = false
-        return arView
-    }()
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        swipeableView.nextView = {
+            return self.nextCardView()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        view.backgroundColor = UIColor.black
+        view.clipsToBounds = true
         
-        view.addSubview(arView)
-
-        arView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        arView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        swipeableView = ZLSwipeableView()
+        view.addSubview(swipeableView)
+        swipeableView.didStart = {view, location in
+            print("Did start swiping view at location: \(location)")
+        }
+        swipeableView.swiping = {view, location, translation in
+            print("Swiping at view location: \(location) translation: \(translation)")
+        }
+        swipeableView.didEnd = {view, location in
+            print("Did end swiping view at location: \(location)")
+        }
+        swipeableView.didSwipe = {view, direction, vector in
+            print("Did swipe view in direction: \(direction), vector: \(vector)")
+        }
+        swipeableView.didCancel = {view in
+            print("Did cancel swiping view")
+        }
+        swipeableView.didTap = {view, location in
+            print("Did tap at location \(location)")
+        }
+        swipeableView.didDisappear = { view in
+            print("Did disappear swiping view")
+        }
         
-        setupCardsView()
+        view.addSubview(swipeableView)
+        swipeableView.translatesAutoresizingMaskIntoConstraints = false
+        swipeableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        swipeableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -50).isActive = true
+        swipeableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 50).isActive = true
+        swipeableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 50).isActive = true
+        setupCustomAnimation()
     }
     
-    private func setupCardsView() {
-        view.addSubview(swipeableView)
-        
-        swipeableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        swipeableView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        
-        swipeableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive = true
-        swipeableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
-        swipeableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 10).isActive = true
-        swipeableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
-
-        swipeableView.numberOfActiveView = 3
-        swipeableView.nextView = {
-            let view = UIView()
-            view.backgroundColor = .red
-            return view
+    private func setupCustomAnimation() {
+        func toRadian(_ degree: CGFloat) -> CGFloat {
+            return degree * CGFloat(M_PI/180)
         }
-        swipeableView.loadViews() // optional, automatically call after nextView is set
+        func rotateAndTranslateView(_ view: UIView, forDegree degree: CGFloat, translation: CGPoint, duration: TimeInterval, offsetFromCenter offset: CGPoint, swipeableView: ZLSwipeableView) {
+            UIView.animate(withDuration: duration, delay: 0, options: .allowUserInteraction, animations: {
+                view.center = swipeableView.convert(swipeableView.center, from: swipeableView.superview)
+                var transform = CGAffineTransform(translationX: offset.x, y: offset.y)
+                transform = transform.rotated(by: toRadian(degree))
+                transform = transform.translatedBy(x: -offset.x, y: -offset.y)
+                transform = transform.translatedBy(x: translation.x, y: translation.y)
+                view.transform = transform
+            }, completion: nil)
+        }
+        swipeableView.numberOfActiveView = 10
+        swipeableView.animateView = {(view: UIView, index: Int, views: [UIView], swipeableView: ZLSwipeableView) in
+            let degree = CGFloat(sin(0.5*Double(index)))
+            let offset = CGPoint(x: 0, y: swipeableView.bounds.height*0.3)
+            let translation = CGPoint(x: degree*10, y: CGFloat(-index*5))
+            let duration = 0.4
+            rotateAndTranslateView(view, forDegree: degree, translation: translation, duration: duration, offsetFromCenter: offset, swipeableView: swipeableView)
+        }
 
-//        var swipeableView = ZLSwipeableView(frame: CGRect(x: 0, y: 0, width: 300, height: 500))
-//        view.addSubview(swipeableView)
-
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: ()
+    func nextCardView() -> UIView? {
+        let cardView = CardView()
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+        swipeableView.addSubview(cardView)
+        cardView.heightAnchor.constraint(equalTo: swipeableView.heightAnchor).isActive = true
+        cardView.widthAnchor.constraint(equalTo: swipeableView.widthAnchor).isActive = true
+        cardView.backgroundColor = .red
+        return cardView
     }
 }
 

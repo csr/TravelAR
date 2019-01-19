cc4cf61ed22e3dcca66ccefa207b8104fb8146d1
Cesare de Cal <contact@cesare.io>
2019-01-19 17:59:06 +0100

    Updated appstore-prebuild script

--- LanguageTranslationAR/app-center-pre-build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo $GOOGLEAPIKEY > $APPCENTER_SOURCE_DIRECTORY/LanguageTranslationAR/API/GoogleAPIKey.txt
-
---
+++ LanguageTranslationAR/appcenter-pre-build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd $APPCENTER_SOURCE_DIRECTORY/LanguageTranslationAR/API/
+echo $GOOGLEAPIKEY > GoogleAPIKey.txt
+
